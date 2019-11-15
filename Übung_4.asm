827eaf608d41e8262615f73f111b63451d387150
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-15 08:41:37 +0100

    Update Übung_4.asm

--- Übung_4.asm
+++ Übung_4.asm
@@ -1,59 +1,41 @@
-
 %include "io.inc"
 
 
 section .data
 
-    ;instead of setting 0h 
-    ;we need to set all 4 bytes to 0, hence the 8 zeroes
-    ; put the numbers you want to add after the zeroes
-
-    first_num  DD 000000255h
-    second_num DD 000000255h
-    
-    ;the product of the two variables will need 8 bytes
-    ; DQ for 8 bytes // DD for 4 bytes
-     
-    product    DQ 0000000000000000h
+first_number  DD 0xABCDEF
+second_number DD 0xFFFFFFFF
+product       DQ 0x0
 
 
 
 section .text
 global CMAIN
 CMAIN:
-    mov ebp, esp; for correct debugging
     ;write your code here
     
-    GET_HEX 4,  [first_num]
-    GET_HEX 4, [second_num]
-    
-    ; Usage of registers starts here, set them to zero to
-    ; clear(overwrite) any other data
-    
-    ; eax for first number
     mov eax, 0
-    ; ebx for second number
     mov ebx, 0
-    ; ecx for possible overflow
+    mov edx, 0
     mov ecx, 0
-   
-    mov ebx, [second_num]
+    mov ecx, [second_number]
     
-    loop_target:
-    add eax, [first_num]
+    addition_loop:
+    add eax, [first_number]
     
-    jnc overflow
-    ; jump not carry if the register would overflow
-    add ecx, 1
-    jmp loop_target
+    jnc no_overflow
+    add edx, 1
     
-    overflow:
+    no_overflow:
+    Loop addition_loop
     
-    mov DWORD [product], eax
-    mov DWORD [product+4], ecx
+    
+    mov DWORD [product],   eax
+    mov DWORD [product+4], edx
     
     PRINT_HEX 4, [product+4]
     PRINT_HEX 4,   [product]
+    
     
     xor eax, eax
     ret
